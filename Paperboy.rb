f85231eda8b751f956178f40eb4e25f684244678
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-15 20:52:56 -0400

    finished paperboy.rb

--- Paperboy.rb
+++ Paperboy.rb
@@ -1,20 +1,30 @@
 class Paperboy
-  def initialize(name, experience)
+  def initialize(name)
     @name = name
-    @experience = experience
-    @earnings
+    @experience = 0
+    @earnings = 0
   end
+
 
 def quota
   50 + @experience/2
 end
 
 def deliver(start_address, end_address)
-  houses = (end_address - start_address + 1).abs
+  delivery_houses = end_address - start_address + 1
+  if delivery_houses <= @quota
+    @earnings = delivery_houses * 0.25
+    if delivery_houses < @quota
+      @earnings -= 2
+    end
+  elsif delivery_houses > @quota
+    overtime = delivery_houses - @quota
+    @earnings = delivery_houses * 0.25 + overtime * 0.5
+  end
 end
 
 def report
-  @report
+  "I'm Tommy, I've delivered #{@quota} papers and I've earned #{@earnings} so far!"
 end
 
 end
